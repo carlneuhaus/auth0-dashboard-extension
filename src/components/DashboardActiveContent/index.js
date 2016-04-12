218bc90c955e610842b45b562938a4cbb169a9ce
mcabral <cabral.martin@gmail.com>
2016-04-12 18:37:10 -0300

    Added datasource object

--- src/components/DashboardActiveContent/index.js
+++ src/components/DashboardActiveContent/index.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 import MetricSelector from '../MetricSelector';
 import MetricContent from '../MetricContent';
 
 import * as MetricKeys from '../../client/metricKeys';
+import DataSource from '../../client/services/dataSource';
 
 var styles = require('./styles.css');
 
 export default class DashboardActiveContent extends Component {
   constructor(props) {
     super(props)
-    this.selectionChanged.bind(this);
+    this.state = {
+      currentMetric: MetricKeys.LOGINS_PER_DAY,
+      isPending: true
+    }
+
+    this.dataSource = new DataSource();
   }
 
-  selectionChanged (e) {
-    console.log('selection changing to', e)
+  selectionChanged (selectedMetric) {
+    this.setState({ isPending: true})
+
+    var dataPromise = this.dataSource.get(selectedMetric);
+    var self = this;
+
+    dataPromise.then(function (data) {
+      self.setState({ isPending: false, dataSet: data});
+    }).fail(function () {
+      console.error('handle error');
+    });
   }
 
   render() {
     return (
       <div className={styles.root  + ' row'}>
         <div className="col-xs-3">
-          <MetricSelector onSelectionChanged={this.selectionChanged}/>
+          <MetricSelector initialMetric={MetricKeys.LOGINS_PER_DAY} onSelectionChanged={this.selectionChanged.bind(this)}/>
         </div>
         <div className="col-xs-9">
           <MetricContent
+            isPending={this.state.isPending}
+            dataSet={this.state.dataSet}
             title="Indentity Providers"
             />
         </div>
