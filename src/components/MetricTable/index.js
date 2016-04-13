958de4b09e991f8a769e99e607eb29e94090d869
mcabral <cabral.martin@gmail.com>
2016-04-13 02:18:52 -0300

    Initial grid styles

--- src/components/MetricTable/index.js
+++ src/components/MetricTable/index.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 
+const styles = require('./styles.css');
+
 export default (props) => (
-  <table className="table data-table">
-    <thead>
-      <tr>
-        <th>Superheroes</th>
-        <th>Superpower</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Superman</td>
-        <td>Flight</td>
-      </tr>
-      <tr>
-        <td>Batman</td>
-        <td>Utility Belt</td>
-      </tr>
-      <tr>
-        <td>@german</td>
-        <td><em>Secret</em></td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <div className={styles.summary}>
+      <span>On detail:</span>
+    </div>
+    <div className={styles.grid}>
+      <table>
+        <thead>
+          <tr>
+            <th>Superheroes</th>
+            <th>Superpower</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Superman</td>
+            <td>Flight</td>
+          </tr>
+          <tr>
+            <td>Batman</td>
+            <td>Utility Belt</td>
+          </tr>
+          <tr>
+            <td>@german</td>
+            <td><em>Secret</em></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 );
