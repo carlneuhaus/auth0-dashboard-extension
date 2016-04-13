08f799a98837d8e90b27c4ae665d82eaa17afa7a
Jason Strutz <jason.strutz@auth0.com>
2016-04-13 14:06:18 -0300

    Added metricInfo

---
+++ src/client/metricInfo.js
@@ -0,0 +1,33 @@
+import * as MetricKeys from './metricKeys';
+
+var info = {};
+info[MetricKeys.LOGINS_PER_DAY] = {
+  title: 'Daily User Login Frequency',
+  explanation: 'Average number of times a user logs in for every day.',
+  tableTitle: 'Most often logged-in user each day'
+}
+
+info[MetricKeys.GEOLOCATION] = {
+  title: 'Geolocation'
+}
+
+info[MetricKeys.LOGINS_PER_SIGNUP] = {
+  title: 'Logins per Signup',
+  explanation: "Ratio of user logins over new signups, indicating the portion of activity from existing users"
+}
+
+info[MetricKeys.LOGS] = {
+  title: 'Logs'
+}
+
+info[MetricKeys.IDENTITY_PROVIDERS] = {
+  title: 'Identity Providers'
+}
+
+info[MetricKeys.CONNECTION_CHURN] = {
+  title: 'User Churn By Connection',
+  explanation: 'Ratio of users joining over new 30-day inactive users',
+  usageHint: ''
+}
+
+export default info;
