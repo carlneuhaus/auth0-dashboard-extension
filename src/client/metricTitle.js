85b07f0d5f0173476bef791484f3f4cf82281f09
Jason Strutz <jason.strutz@auth0.com>
2016-04-12 23:29:10 -0300

    Added metric keys -> metric title mapping function

---
+++ src/client/metricTitle.js
@@ -0,0 +1,16 @@
+import * as MetricKeys from './metricKeys';
+
+export default function(key) {
+  switch(key) {
+    case MetricKeys.LOGINS_PER_DAY:
+      return 'Logins Per Day';
+    case MetricKeys.GEOLOCATION:
+      return 'Geolocation';
+    case MetricKeys.LOGINS_PER_SIGNUP:
+      return 'Logins Per Signup';
+    case MetricKeys.LOGS:
+      return 'Logs';
+    case MetricKeys.IDENTITY_PROVIDERS:
+      return 'Identity Providers';
+  }
+};
